71a2ac7e03939bc8592f80f38eb99aaf8930dea6
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-10-06 19:08:27 -0500

    feat: add Ejercicio1

---
+++ app/src/main/java/com/josuerdx/sem9_ejercicio1/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.josuerdx.sem9_ejercicio1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.josuerdx.sem9_ejercicio1.ui.theme.Sem9_Ejercicio1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Sem9_Ejercicio1Theme {
+
+            }
+        }
+    }
+}
+
